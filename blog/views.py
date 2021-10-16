625416e1c77b3cc7206f690fb667c79bf4b69a69
lesley ayemi <leeayemi@gmail.com>
2021-10-16 22:28:59 +0100

    mades changes to the email

--- blog/views.py
+++ blog/views.py
@@ -228,7 +228,7 @@ def contact(request):
             to_email = ('no-reply@blesidiary.com', '')
         if  subject and message and from_email:
             try:
-                send_mail(subject, message, from_email, ['no-reply@blesidiary.com'])
+                send_mail(subject, message, from_email, ['info@blesidiary.com'])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return HttpResponseRedirect('/contact')
